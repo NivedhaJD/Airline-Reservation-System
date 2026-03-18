ab7ead86ed42fd66e4d796d933f3c17c7e5dafe3
Nivedha <jn5681@srmist.edu.in>
2026-03-18 20:23:10 +0530

    Change app.run to use host and port

--- app.py
+++ app.py
@@ -550,4 +550,4 @@ def get_meals(meal_type):
 # MAIN ENTRY POINT
 # ============================================================
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
